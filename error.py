d3d09df6d712a3628b98fbf609851859539becf5
tatekitzhak <tatekosi@gmail.com>
2022-01-24 10:46:08 +0200

    Added a new file.

---
+++ error.py
@@ -0,0 +1,54 @@
+******** https://www.youtube.com/watch?v=zOjov-2OZ0E
+Removing cache dir /Users/eli/.cache/youtube-dl ..
+[youtube] zOjov-2OZ0E: Downloading webpage
+[youtube] Downloading just video zOjov-2OZ0E because of --no-playlist
+[youtube] zOjov-2OZ0E: Downloading webpage
+[youtube] Downloading just video zOjov-2OZ0E because of --no-playlist
+ERROR: unable to download video data: HTTP Error 403: Forbidden
+
+
+
+******** https://www.youtube.com/watch?v=CHhwJjR0mZA
+Removing cache dir /Users/eli/.cache/youtube-dl ..
+[youtube] CHhwJjR0mZA: Downloading webpage
+[youtube] Downloading just video CHhwJjR0mZA because of --no-playlist
+[youtube] CHhwJjR0mZA: Downloading webpage
+[youtube] Downloading just video CHhwJjR0mZA because of --no-playlist
+[download] Destination: /Users/eli/git_repos/python-script-extract-yt-video-data/test/CHhwJjR0mZA.wav
+[download]  31.6% of 46.57MiB at 73.95KiB/s ETA 07:21ERROR: giving up after 0 retries
+
+******** https://www.youtube.com/watch?v=09_LlHjoEiY
+Removing cache dir /Users/eli/.cache/youtube-dl ..
+[youtube] 09_LlHjoEiY: Downloading webpage
+[youtube] Downloading just video 09_LlHjoEiY because of --no-playlist
+[youtube] 09_LlHjoEiY: Downloading webpage
+[youtube] Downloading just video 09_LlHjoEiY because of --no-playlist
+[download] Destination: /Users/eli/git_repos/python-script-extract-yt-video-data/test/09_LlHjoEiY.wav
+[download]  16.2% of 374.73MiB at 69.78KiB/s ETA 01:16:49ERROR: giving up after 0 retries
+
+******** https://www.youtube.com/watch?v=-W_VsLXmjJU
+Removing cache dir /Users/eli/.cache/youtube-dl ..
+[youtube] -W_VsLXmjJU: Downloading webpage
+[youtube] Downloading just video -W_VsLXmjJU because of --no-playlist
+[youtube] -W_VsLXmjJU: Downloading webpage
+[youtube] Downloading just video -W_VsLXmjJU because of --no-playlist
+[download] Destination: /Users/eli/git_repos/python-script-extract-yt-video-data/test/-W_VsLXmjJU.wav
+[download]  75.5% of 10.71MiB at 74.40KiB/s ETA 00:36ERROR: giving up after 0 retries
+
+******** https://www.youtube.com/watch?v=sbbYntt5CJk
+Removing cache dir /Users/eli/.cache/youtube-dl ..
+[youtube] sbbYntt5CJk: Downloading webpage
+[youtube] Downloading just video sbbYntt5CJk because of --no-playlist
+[youtube] sbbYntt5CJk: Downloading webpage
+[youtube] Downloading just video sbbYntt5CJk because of --no-playlist
+[download] Destination: /Users/eli/git_repos/python-script-extract-yt-video-data/test/sbbYntt5CJk.wav
+[download]   7.3% of 647.89MiB at 72.82KiB/s ETA 02:20:42ERROR: giving up after 0 retries
+
+******** https://www.youtube.com/watch?v=D6Q_wHrzxDs
+Removing cache dir /Users/eli/.cache/youtube-dl ..
+[youtube] D6Q_wHrzxDs: Downloading webpage
+[youtube] Downloading just video D6Q_wHrzxDs because of --no-playlist
+[youtube] D6Q_wHrzxDs: Downloading webpage
+[youtube] Downloading just video D6Q_wHrzxDs because of --no-playlist
+[download] Destination: /Users/eli/git_repos/python-script-extract-yt-video-data/test/D6Q_wHrzxDs.wav
+[download]   1.5% of 666.20MiB at 50.64KiB/s ETA 03:41:03ERROR: giving up after 0 retries
