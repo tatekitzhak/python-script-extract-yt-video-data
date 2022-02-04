ffcfc054c6dd2786f57f7d70cc7bc8c680c18c51
tatekitzhak <tatekosi@gmail.com>
2022-02-04 16:26:15 +0200

    Update a script, ignore folder and added a feature that handling errors and exceptions.

--- create_file.py
+++ create_file.py
@@ -3,21 +3,23 @@ import os
 
 ROOT_DIR  = os.path.dirname(os.path.abspath(__file__))
 
-
-
 def create_file(folder_name,subfolder_name):
 	try:
-		files = ['file_a', 'file_b', 'file_c']
+		files = ['file_a', 'file_b', 'file_c','file_d']
 		for i, file in enumerate(files):
 			file_name = file + str(i)
 			myfile = Path(ROOT_DIR+'/'+folder_name+'/'+subfolder_name+'/'+file_name+'.txt')
 			myfile.touch(exist_ok=True)
 			open(myfile)
-			print(file, len(file))
+			print('Create a new file at path:', myfile)
 	except Exception as e:
-	    print(e)
-	
-	
-		
+		print('Error:',e)
+		raise
+	else:
+		print('Result is:',file_name)
+	finally:
+		print('executing finally:',myfile)
 
+
+	
 create_file('topic','subtopic')
