1d21d642f520990e2cfbf57970fdabb0c801d6c1
tatekitzhak <tatekosi@gmail.com>
2022-02-17 16:06:43 +0200

    update a s3.py file script.

--- s3/s3.py
+++ s3/s3.py
@@ -1,8 +1,15 @@
 import boto3
+import os
+from dotenv import load_dotenv
+load_dotenv()
 
-s3 = boto3.resource('s3')
-ec2 = boto3.resource('ec2')
-iam = boto3.resource('iam')
+
+S3_ACCESS_KEY =  os.getenv("S3AccessKeyID")
+s3_SECRET_KEY =  os.getenv("S3SecretAccessKey")
+
+s3 = boto3.resource('s3',aws_access_key_id=S3_ACCESS_KEY,aws_secret_access_key=s3_SECRET_KEY)
 
 for each_bucket in s3.buckets.all():
-	print('s3:',each_bucket)
+	print('s3:',each_bucket.name)
+
+
